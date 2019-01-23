7284e0c6d7255b971d79e8380fe7f15ec8a73538
Alessandro Rodi <alessandro.rodi@renuo.ch>
2019-01-23 12:49:55 +0100

    Remove failing overrides

--- app/overrides/add_extra_order_details_line_item_description.rb
+++ app/overrides/add_extra_order_details_line_item_description.rb
@@ -5,9 +5,10 @@ Deface::Override.new(
   partial: 'spree/shared/extra_order_details_line_item_description'
 )
 
-Deface::Override.new(
-  virtual_path: 'spree/admin/shared/_order_details',
-  name: 'add_extra_order_details_line_item_description_admin',
-  insert_bottom: "[data-hook='order_details_line_item_row'] td:first",
-  partial: 'spree/shared/extra_order_details_line_item_description'
-)
+# TODO: not present in latest solidus
+# Deface::Override.new(
+#   virtual_path: 'spree/admin/shared/_order_details',
+#   name: 'add_extra_order_details_line_item_description_admin',
+#   insert_bottom: "[data-hook='order_details_line_item_row'] td:first",
+#   partial: 'spree/shared/extra_order_details_line_item_description'
+# )
